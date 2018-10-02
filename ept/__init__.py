4262738f71b947da866b20c8d7be92be65b8aaa2
tmontaigu <thomas.montaigu@laposte.net>
2018-10-02 13:51:07 +0200

    add setup.py

---
+++ ept/__init__.py
@@ -0,0 +1,1 @@
+from .eptresource import EPTResource, SyncEPTResource
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='ept',
+    version='0.0.1',
+    packages=['ept', 'ept.sources'],
+    url='',
+    license='MIT',
+    author='t.montaigu',
+    author_email='',
+    description=''
+)
